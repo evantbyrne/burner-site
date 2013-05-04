43e43e894ae6701c5976c00fd04264dd8e409039
Evan Byrne <evantbyrne@gmail.com>
2013-05-04 13:59:31 -0400

    Made it so that display_name on User model must be unique

--- app/model/user.php
+++ app/model/user.php
@@ -11,6 +11,7 @@ class User extends \Library\Auth\Model\MultipleGroups {
 	 * @option type = Varchar
 	 * @option length = 25
 	 * @option required = Display name field required.
+	 * @option unique = Display name already taken. Please choose another.
 	 */
 	public $display_name;
 	
