df56589b37d2f32d8db0c214d61595afa82aef80
Evan Byrne <evantbyrne@gmail.com>
2013-05-20 20:11:46 -0400

    Added .ico

--- app/template/base.php
+++ app/template/base.php
@@ -11,6 +11,7 @@
 		<link rel="stylesheet" type="text/css" href="<?= url('static/style/bootstrap/css/bootstrap.min.css'); ?>" />
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" type="image/x-icon" href="<?= url('static/style/burner.ico'); ?>" />
 
 		<!--[if lt IE 9]>
 			<script>
